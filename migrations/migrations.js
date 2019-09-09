707936b48777669564954a7e883da1cac51572fd
Joshua Melville <joshua.melville@northwestern.edu>
2019-09-09 14:29:40 -0400

    wip

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -1,6 +1,11 @@
 const migrations = [
   { version: '1.0.0', migration: noop => noop },
   { version: '1', migration: noop => noop },
+  { version: '2', migration: protocol => ({
+    ...protocol,
+    schemaVersion: 2,
+    melvilleFactor: 5,
+  })},
 ];
 
 module.exports = migrations;
